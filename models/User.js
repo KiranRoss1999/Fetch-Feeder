4f1af89bb5c088d3611f3138a0765024969197af
Pradeep Khanal <pradeepkhanal642@gmail.com>
2024-06-11 20:59:35 +1000

    starter code including models, tailwind, package json and server

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
 
 class User extends Model {
   checkPassword(loginPw) {
@@ -9,17 +9,17 @@ class User extends Model {
 }
 
 User.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -45,35 +45,38 @@ User.init(
         // }
       },
     },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [8],
-        },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
       },
     },
-    {
-      hooks: {
-        beforeCreate: async (newUserData) => {
-          newUserData.password = await bcrypt.hash(newUserData.password, 10);
-          newUserData.email = newUserData.email.toLowerCase();
-          return newUserData;
-        },
-        beforeUpdate: async (updatedUserData) => {
-          if (updatedUserData.changed('password')) {
-            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-          }
-          updatedUserData.email = updatedUserData.email.toLowerCase();
-          return updatedUserData;
-        }, 
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        newUserData.email = newUserData.email.toLowerCase();
+        return newUserData;
       },
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'user',
-    }
-  );
-  
-  module.exports = User;
+      beforeUpdate: async (updatedUserData) => {
+        if (updatedUserData.changed("password")) {
+          updatedUserData.password = await bcrypt.hash(
+            updatedUserData.password,
+            10
+          );
+        }
+        updatedUserData.email = updatedUserData.email.toLowerCase();
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
---
+++ seeds/seedMeals.js
@@ -0,0 +1,19 @@
+const sequelize = require("../config/connection");
+const { MealLog } = require("../models");
+
+const mealsData = require("./mealsData.json");
+
+const seedMeals = async () => {
+  try {
+    await sequelize.sync({ force: true });
+    await MealLog.bulkCreate(mealsData, {
+      individualHooks: true,
+      returning: true,
+    });
+    console.log("MealLogs seeded");
+  } catch (error) {
+    console.error("Failed to seed meal logs:", error);
+  }
+};
+
+module.exports = seedMeals;
