4c3174b62bc2022dd58d755bf2e25e9e6f0895bf
KiranRoss <120018062+KiranRoss@users.noreply.github.com>
2024-06-18 23:36:18 +1000

    render deployment retry

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,23 @@ require("dotenv").config();
 
 let sequelize;
 
-if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
+if (process.env.NODE_ENV === 'production') {
+  //sequelize = new Sequelize(process.env.DB_URL);
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD, // Ensure this matches your .env file
+    {
+      host: process.env.DB_HOST,
+      dialect: "postgres",
+      dialectOptions: {
+        "ssl": {
+          "require": true,
+          "rejectUnauthorized": false
+        }
+      }
+    }
+  );
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
