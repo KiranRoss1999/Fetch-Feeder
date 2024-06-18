2070370faa0f6383f5331acef8f48f0367c96439
KiranRoss <120018062+KiranRoss@users.noreply.github.com>
2024-06-18 22:17:26 +1000

    connect to postgresql

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
+// config/connection.js
+
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: "localhost",
-        dialect: "postgres",
-      }
-    );
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD, // Ensure this matches your .env file
+    {
+      host: "localhost",
+      dialect: "postgres",
+    }
+  );
+}
 
 module.exports = sequelize;
